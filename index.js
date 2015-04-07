28f81253e58594d2edfd398046956573a68c1f13
yukinagae <yuki.nagae1130@gmail.com>
2015-04-07 13:46:37 +0900

    implement GET users method only.

---
+++ index.js
@@ -0,0 +1,53 @@
+// import
+var request = require('request');
+
+// setting
+var base_url = 'http://qiita.com/api/v2/';
+var apis = ['users', 'items', 'comments'];
+
+// Qiita4js object
+function Qiita4js() {
+}
+
+// available apis
+apis.forEach(function(api) {
+  Qiita4js.prototype[api] = function(param) {
+    var url = base_url;
+    if(param) {
+      url += api + '/' + param; // ex) http://qiita.com/api/v2/users/yukinagae
+    } else {
+      url += api + '/'; // ex) http://qiita.com/api/v2/users/
+    }
+    return new Promise(function(resolve, reject) {
+      request(url, function(error, response, body) {
+          if(error || response.statusCode !== 200) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body));
+          }
+      });
+    });
+  };
+});
+
+// test
+var qiita = new Qiita4js();
+var promise = qiita.users('yukinagae');
+promise.then(function(result) {
+  console.log(result); // "Stuff worked!"
+}, function(err) {
+  console.log(err); // Error: "It broke"
+});
+
+// export
+module.exports = Qiita4js;
+
+// TODO
+// - get user
+// - get item
+// - get comment
+// - list users
+// - list items
+// - list tag items
+// - list user items
+// - list item comments
