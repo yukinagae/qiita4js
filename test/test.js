4fd7ac8c7a252328f1cdb4505d6da30664799c7c
yukinagae <yuki.nagae1130@gmail.com>
2015-04-08 09:19:04 +0900

    remove some duplicate comments.

--- test/test.js
+++ test/test.js
@@ -18,73 +18,64 @@ describe('Qiita4js', function() {
     Qiita4js.should.be.type('function');
   });
 
-  // - get user
   // GET /api/v2/users/:id
-  it('should request 1 user', function() {
+  it('should get 1 user', function() {
     var qiita = new Qiita4js(test);
     var url = qiita.users('yukinagae');
     assert.equal(url, 'http://qiita.com/api/v2/users/yukinagae');
   });
 
-  // - list users
   // GET /api/v2/users
-  it('should request first 20 users', function() {
+  it('should list first 20 users', function() {
     var qiita = new Qiita4js(test);
     var url = qiita.users();
     assert.equal(url, 'http://qiita.com/api/v2/users');
   });
 
-  // - get item
   // GET /api/v2/items/:id
-  it('should request 1 item', function() {
+  it('should get 1 item', function() {
     var qiita = new Qiita4js(test);
     var url = qiita.items('d2de7a3c0451cc96aa00');
     assert.equal(url, 'http://qiita.com/api/v2/items/d2de7a3c0451cc96aa00');
   });
 
-  // - list items
   // GET /api/v2/items
-  it('should request first 20 items', function() {
+  it('should list first 20 items', function() {
     var qiita = new Qiita4js(test);
     var url = qiita.items();
     assert.equal(url, 'http://qiita.com/api/v2/items');
   });
 
-  // - list user items
   // GET /api/v2/users/:user_id/items
-  it('should request user items', function() {
+  it('should list user items', function() {
     var qiita = new Qiita4js(test);
     var url = qiita.users_items('yukinagae');
     assert.equal(url, 'http://qiita.com/api/v2/users/yukinagae/items');
   });
 
-  // - list stocked items
   // GET /api/v2/users/:user_id/stocks
-  it('should request user stocked items', function() {
+  it('should list user stocked items', function() {
     var qiita = new Qiita4js(test);
     var url = qiita.users_stocks('yukinagae');
     assert.equal(url, 'http://qiita.com/api/v2/users/yukinagae/stocks');
   });
 
-  // - list tag items
   // GET /api/v2/tags/:id/items
-  it('should request tag items', function() {
+  it('should list tag items', function() {
     var qiita = new Qiita4js(test);
     var url = qiita.tags_items('node.js');
     assert.equal(url, 'http://qiita.com/api/v2/tags/node.js/items');
   });
 
-  // - get comment
   // GET /api/v2/comments/:id
-  it('should request 1 comment', function() {
+  it('should get 1 comment', function() {
     var qiita = new Qiita4js(test);
     var url = qiita.comments('ba0a1b50b9803f38d200');
     assert.equal(url, 'http://qiita.com/api/v2/comments/ba0a1b50b9803f38d200');
   });
 
-  // - list item comments
   // GET /api/v2/items/:item_id/comments
-  it('should request item comments', function() {
+  it('should list item comments', function() {
     var qiita = new Qiita4js(test);
     var url = qiita.items_comments('d2de7a3c0451cc96aa00');
     assert.equal(url, 'http://qiita.com/api/v2/items/d2de7a3c0451cc96aa00/comments');
